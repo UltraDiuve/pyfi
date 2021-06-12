9ab4f292d5432aa15a1e440dea87c1ede66c660b
UltraDiuve <pit.masse@gmail.com>
2021-06-12 21:30:57 +0200

    experiment with backtrader

--- services/strategies/strategies.py
+++ services/strategies/strategies.py
@@ -1,6 +1,7 @@
-from os import truncate
 import pandas as pd
 import datetime as dt
+import matplotlib.pyplot as plt
+from typing import List
 
 
 class Signal(object):
@@ -27,28 +28,25 @@ class VirtualPortfolio(object):
     def __init__(
         self,
         initial_volumes=None,
-        datetimes=None,  
+        datetimes=None,
+        fee_rate=0.0026,
     ) -> None:
         self.assets = pd.DataFrame(
             initial_volumes,
             columns=list(initial_volumes.keys()),
             index=datetimes,
         ).astype(float)
-    
+        self.fees = pd.Series(
+            0.,
+            index=datetimes,
+        )
+        self.fee_rate = fee_rate
+
     def add_asset(
         self,
         asset_code,
         initial_volume=0.,
     ):
-        # if not self.asset_exists(asset_code):
-        #     self.assets = pd.concat([
-        #         self.assets,
-        #         pd.DataFrame(
-        #             [[asset_code, float(initial_volume)]],
-        #             columns=['asset_code', 'volume']    
-        #         )
-        #     ]
-        # )
         if self.asset_exists(asset_code):
             raise RuntimeError(f"Asset {asset_code} already exists!")
         self.assets[asset_code] = float(initial_volume)
@@ -81,7 +79,7 @@ class VirtualPortfolio(object):
     ) -> None:
         if not datetime:
             datetime = self.assets.index.max()
-        
+
         value_from = 0.
         if increment:
             value_from = self.get_asset_current_volume(
@@ -89,18 +87,17 @@ class VirtualPortfolio(object):
                 datetime=datetime,
             )
             new_volume = value_from + volume
-        # if not allow_short_sell and new_volume < 0.:
-        #     raise RuntimeError('Short selling whereas flag has not been set.')
         self.assets.loc[datetime:, asset_code] = new_volume
 
     def trade(
         self,
         asset_bought: str,
         asset_sold: str,
-        price: float=None,
-        volume_sold: float=None,
-        allow_short_sale: bool=False,
+        price: float = None,
+        volume_sold: float = None,
+        allow_short_sale: bool = False,
         datetime=None,
+        fees_ratio=0.0026,
     ):
         if not volume_sold:
             volume_sold = self.get_asset_current_volume(
@@ -108,12 +105,14 @@ class VirtualPortfolio(object):
                 datetime=datetime,
             )
 
-        if volume_sold > self.get_asset_current_volume(
-                asset_sold,
-                datetime=datetime,
-            ) and not allow_short_sale:
+        if (
+                volume_sold > self.get_asset_current_volume(
+                    asset_sold,
+                    datetime=datetime,
+                ) and not allow_short_sale
+        ):
             raise RuntimeError('Short selling whereas flag has not been set.')
-        
+
         self.update_asset_volume(
             asset_sold,
             volume=-volume_sold,
@@ -128,27 +127,59 @@ class VirtualPortfolio(object):
             datetime=datetime,
         )
 
+        # should be updated to take care of case fee is applied to trade
+        # without asset being EUR
+        if asset_bought == 'EUR':
+            trade_value = volume_sold * price
+        elif asset_sold == 'EUR':
+            trade_value = volume_sold
+        else:
+            raise NotImplementedError(
+                "fee computation to be implemented."
+            )
+        self.update_fees(
+            trade_value=trade_value,
+            overriden_fee_rate=None,
+            datetime=datetime,
+        )
+
+    def update_fees(
+        self,
+        trade_value=0.,
+        overriden_fee_rate=None,
+        datetime=None,
+    ):
+        if not datetime:
+            raise ValueError('datetime unspecified for fee computation')
+        if overriden_fee_rate:
+            fee_rate = overriden_fee_rate
+        else:
+            fee_rate = self.fee_rate
+        self.fees.loc[datetime] += trade_value * fee_rate
+
     def pretty_trade(
         self,
         base_asset_code: str,
         quote_asset_code: str,
-        market_price: bool=False,
-        price: float=None,
-        volume: float=None,
-        allow_short_sale: bool=False,
-        trade_type: str='buy',
+        market_price: bool = False,
+        price: float = None,
+        volume: float = None,
+        allow_short_sale: bool = False,
+        trade_type: str = 'buy',
         datetime=None,
+        verbose=0,
     ) -> None:
         if market_price:
-            raise NotImplemented("Market price service to be integrated")
+            raise NotImplementedError("Market price service to be integrated")
 
         if not market_price and not price:
             raise ValueError('Price not supplied for trade')
 
-        print(
-            f'Trading: {trade_type} {base_asset_code} {quote_asset_code}'
-            f' at price {price} for volume {volume}'
-        )
+        if verbose >= 2:
+            print(
+                f'Trading: {trade_type} {base_asset_code} {quote_asset_code}'
+                f' at price {price} for volume {volume}'
+            )
 
         volume_sold = None
 
@@ -161,12 +192,12 @@ class VirtualPortfolio(object):
         elif trade_type == 'sell':
             asset_sold = base_asset_code
             asset_bought = quote_asset_code
-            price = 1 /price
+            price = 1 / price
             if volume:
                 volume_sold = volume
         else:
             raise ValueError(f'Unexpected type:{type}')
-        
+
         self.trade(
             asset_bought=asset_bought,
             asset_sold=asset_sold,
@@ -176,72 +207,69 @@ class VirtualPortfolio(object):
             datetime=datetime,
         )
 
-        # if trade_type == 'buy':
-        #     sign = 1.
-        # elif trade_type == 'sell':
-        #     sign = -1.
-        # else:
-        #     raise ValueError(f'Unexpected type:{type}')
-
-        # if not volume:
-        #     if trade_type == 'buy':
-        #         volume = self.get_asset_current_volume(quote_asset_code) / price
-        #     if trade_type == 'sell':
-        #         volume = self.get_asset_current_volume(base_asset_code)
-        #     print(f'Computed volume is {volume}')
-
-
-
-        # if not self.asset_exists(base_asset_code):
-        #     self.add_asset(base_asset_code)
-        # if not self.asset_exists(quote_asset_code):
-        #     self.add_asset(quote_asset_code)
-        # volume = volume * sign
-        # total_price = price * volume
-        # self.update_asset_volume(
-        #     base_asset_code,
-        #     volume,
-        #     increment=True,
-        #     allow_short_sell=allow_short_sell,
-        # )
-        # self.update_asset_volume(
-        #     quote_asset_code,
-        #     total_price * -1,
-        #     increment=True,
-        #     allow_short_sell=allow_short_sell,
-        # )
-
     def __repr__(self):
         return(repr(self.assets))
 
-    def eval_value(
-        self,
-        prices=None,
-        quote_asset='EUR',
-        date_of_value=None,
-    ):
-        eval = self.assets.set_index('asset_code').join(prices.rename('price'))
-        eval['value'] = eval['volume'] * eval['price']
-        return(eval['value']).sum()
-
-    def eval_values(
+    def historic_valorisation(
         self,
         prices_history=None,
-        quote_asset='EUR',    
+        quote_asset='EUR',
     ):
-        pass
-        ### TO BE CONTINUED... Should have portfolio by date ?
+
+        portfolio_values = pd.DataFrame(index=prices_history.index)
+
         # check if all current assets have prices in quote asset
-        # for asset in self.assets['asset'].unique():
-        #     if (
-        #         asset + quote_asset not in prices_history.columns and
-        #         asset != quote_asset
-        #     ):
-        #         raise RuntimeError(f"asset {asset} missing in prices history")
-            
-        # portfolio_values = pd.DataFrame(index=prices_history.index)
-        # for asset in self.assets['asset'].unique():
-        #     if asset == quote_asset:
+        for asset in self.assets.columns:
+            if (
+                asset + quote_asset not in prices_history.columns and
+                asset != quote_asset
+            ):
+                raise RuntimeError(f"asset {asset} missing in prices history")
+
+        portfolio_values = pd.DataFrame(index=prices_history.index)
+        portfolio_values[quote_asset] = self.assets[quote_asset]
+        for asset in self.assets.columns:
+            if asset == quote_asset:
+                continue
+            portfolio_values[asset] = (
+                prices_history[asset + quote_asset] * self.assets[asset]
+            )
+        return(portfolio_values)
+
+    def eval_performance(
+        self,
+        prices_history=None,
+        quote_asset='EUR',
+    ):
+        values = self.historic_valorisation(
+            prices_history=prices_history,
+        ).sum(axis=1)
+        return_ratio = values.iloc[-1] / values.iloc[0] - 1
+        print(f"Return on period is: {return_ratio:.2%}")
+        test_duration = self.assets.index.max() - self.assets.index.min()
+        annualized_return = (
+            (1 + return_ratio) ** (dt.timedelta(days=365) / test_duration) - 1
+        )
+        print(f"Annualized return on period is: {annualized_return:.2%}")
+        total_fees = self.fees.sum()
+        net_return_ratio = (values.iloc[-1] - total_fees) / values.iloc[0] - 1
+        net_annualized_return = (
+            (1 + net_return_ratio) ** (
+                (dt.timedelta(days=365) / test_duration)
+            ) - 1
+        )
+        print(f"Fees for trades are: {total_fees}")
+        print(f"Net return on period is: {net_return_ratio:.2%}")
+        print(
+            f"Annualized net return on period is: {net_annualized_return:.2%}"
+        )
+        return({
+            'return_ratio': return_ratio,
+            'annualized_return_ratio': annualized_return,
+            'total_fees': total_fees,
+            'net_return_ratio': net_return_ratio,
+            'net_annualized_return_ratio': net_annualized_return,
+        })
 
 
 class CrossAverageStrategy(object):
@@ -250,87 +278,105 @@ class CrossAverageStrategy(object):
     def __init__(
         self,
         trading_pair=None,
-        profit_save_rate=0.,
+        # profit_save_rate=0.,
+        long_window=20,
+        short_window=5,
     ) -> None:
-        self.profit_save_rate = profit_save_rate
+        # self.profit_save_rate = profit_save_rate
         self.trading_pair = trading_pair
+        self.long_window = long_window
+        self.short_window = short_window
 
     def generate_signals(
         self,
         data,
-    ) -> list[Signal]:
-        # this should be overriden in subclasses
-        return(
-            [
+        create_viz=False,
+    ) -> List[Signal]:
+        # here, data is a timeseries with asset price.
+        price_series = data.loc[:, self.trading_pair]
+        short_mv = price_series.rolling(self.short_window).mean(center=False)
+        long_mv = price_series.rolling(self.long_window).mean(center=False)
+        buys = (short_mv > long_mv) & ~(short_mv > long_mv).shift(1).iloc[1:]
+        sells = ~(short_mv > long_mv) & (short_mv > long_mv).shift(1).iloc[1:]
+        idxs = buys | sells
+        signals = []
+        for datetime in idxs.loc[idxs].index:
+            if buys[datetime]:
+                signal_type = 'buy'
+            elif sells[datetime]:
+                signal_type = 'sell'
+            else:
+                raise RuntimeError('Unexpected buy / sell type')
+            signals.append(
                 Signal(
                     base_asset='BTC',
                     quote_asset='EUR',
-                    signal_type='buy',
-                    datetime=pd.to_datetime('2020-01-01'),
+                    signal_type=signal_type,
+                    datetime=datetime,
                     volume=None,
-                ),
-                Signal(
-                    base_asset='BTC',
-                    quote_asset='EUR',
-                    signal_type='sell',
-                    datetime=pd.to_datetime('2020-01-04'),
-                    volume=None,
-                ),
-                Signal(
-                    base_asset='BTC',
-                    quote_asset='EUR',
-                    signal_type='buy',
-                    datetime=pd.to_datetime('2020-01-05'),
-                    volume=None,
-                ),
-            ]
-        )
-        # raise(NotImplementedError)
+                )
+            )
+
+        if create_viz:
+            fig, ax = plt.subplots(figsize=(20, 20))
+            ax.plot(price_series)
+            ax.plot(short_mv)
+            ax.plot(long_mv)
+            ax.scatter(
+                buys[buys].index,
+                price_series[buys[buys].index],
+                marker="^",
+                s=200,
+                color='green',
+            )
+            ax.scatter(
+                sells[sells].index,
+                price_series[sells[sells].index],
+                marker="v",
+                s=200,
+                color='red',
+            )
+
+        return(signals)
 
     def evaluate(
         self,
         price_history=None,
-        initial_portfolio: VirtualPortfolio=None,
+        initial_portfolio: VirtualPortfolio = None,
+        create_viz=False,
         # initial_free_value=None,
     ) -> None:
         portfolio = initial_portfolio
-        price_history = price_history[self.trading_pair]
-        initial_portfolio_value = portfolio.eval_value(
-            prices = pd.Series({
-                'EUR': 1.0,
-                'BTC': 100.,
-            }),
-        )
+        # price_history = price_history[self.trading_pair]
+        # initial_portfolio_value = portfolio.eval_value(
+        #     prices = pd.Series({
+        #         'EUR': 1.0,
+        #         'BTC': 100.,
+        #     }),
+        # )
         # free_value = initial_free_value
-        signals = self.generate_signals(price_history)
+        signals = self.generate_signals(
+            price_history,
+            create_viz=create_viz,
+        )
+
         if signals[0].signal_type == "sell":
             signals = signals[1:]
         for signal in signals:
-            price = price_history[signal.datetime]
+            # print(price_history)
+            price = price_history.loc[signal.datetime, self.trading_pair]
             # print(portfolio)
-            portfolio.trade(
+            portfolio.pretty_trade(
                 signal.base_asset,
                 signal.quote_asset,
-                price=price_history[signal.datetime],
+                price=price,
                 volume=None,
-                allow_short_sell=False,
+                allow_short_sale=False,
                 trade_type=signal.signal_type,
+                datetime=signal.datetime,
             )
-        final_prices = pd.Series(
-            {
-                'EUR': 1.,
-                'BTC': price_history.iloc[-1],
-            }
+        return(
+            portfolio.eval_performance(
+                prices_history=price_history,
+            )
         )
-        final_value = portfolio.eval_value(final_prices)
-        print(
-            f"Evaluation of porfolio is: "
-            f"{final_value}")
-        return_ratio = (
-            (final_value - initial_portfolio_value) / initial_portfolio_value
-        )
-        test_duration = price_history.index.max() - price_history.index.min()
-        annualized_return = (1 + return_ratio) ** (dt.timedelta(days=365) / test_duration) - 1
-        print(f"Percent return is: {return_ratio:.2%}")
-        print(f"Annualized return is: {annualized_return:.2%}")
-        return(portfolio)
